f6a4b2af7a7c2f59cdb72a25bd350ddb2ea6ac88
Keven Beauchamp <148033060+KevenBeauchamp@users.noreply.github.com>
2025-02-03 23:38:15 -0500

    Update main.jsx deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'https://kevenbeauchamp.github.io/portfolioKWI/index.css'
-import App from 'https://kevenbeauchamp.github.io/portfolioKWI/App.jsx'
+import App from 'https://kevenbeauchamp.github.io/portfolioKWI/App.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
