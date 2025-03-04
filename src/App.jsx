329015e050bfa343498b375f810b3f6060fc509a
Keven Beauchamp <kevenbeauchamp96@gmail.com>
2025-03-04 01:53:41 -0500

    part 1 responsive

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import SkillsPage from './pages/Skills';
 import ResumePage from './pages/Resume';
 import ContactPage from './pages/Conatct';
 import ProjectPage from './pages/Project';
+import Exemple from './pages/Exemple';
+import { useMediaQuery } from 'react-responsive'
+
 
 function App() {
   const [data, setData] = useState(null);
@@ -36,8 +39,36 @@ function App() {
         }
     ]
 );
+const isDesktopOrLaptop = useMediaQuery({
+  query: '(min-width: 1224px)'
+})
+const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
   return (
-    <RouterProvider router={routers} />
+    <div>
+      {isDesktopOrLaptop && <RouterProvider router={routers} />}
+      {isBigScreen && <p>You have a huge screen</p>}
+      {isTabletOrMobile && 
+        <>
+          <HomePage />
+          <div>
+            <h2>About me</h2>
+            <AboutPage />
+          </div>
+          <div>
+            <h2>My Skills</h2>
+            <SkillsPage />
+          </div>          
+          <ProjectPage />
+          <ResumePage />
+          <ContactPage />
+        </>
+      }
+      {/* <p>Your are in {isPortrait ? 'portrait' : 'landscape'} orientation</p> */}
+      {/* {isRetina && <p>You are retina</p>} */}
+    </div>
   )
 }
 
