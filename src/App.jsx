ec58c8604d76c7261b0c6e1e75cc18d99044c427
Keven Beauchamp <kevenbeauchamp96@gmail.com>
2025-03-06 23:34:49 -0500

    add nav in responsive

--- src/App.jsx
+++ src/App.jsx
@@ -52,19 +52,47 @@ const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
       {/* {isBigScreen && <p>You have a huge screen</p>} */}
       {isTabletOrMobile && 
         // <>
-          <div className='content'>         
+          <div className='content'> 
+            <div className='navbar'>
+              <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skill">Skills</a></li>
+                <li><a href="#project">Project</a></li>
+                <li><a href="#resume">Resume</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </div> 
+            <section id='home'>
             <HomePage />
-            <div>
-              <h2 className='center'>About me</h2>
-              <AboutPage />
-            </div>
-            <div>
-              <h2 className='center'>My Skills</h2>
-              <SkillsPage />
-            </div>          
-            <ProjectPage />
-            <ResumePage />
-            <ContactPage />
+
+            </section>       
+            <section id='about'>
+              <div>
+                <h2 className='center'>About me</h2>
+                <AboutPage />
+              </div>
+            </section>       
+            <section id='skils'>
+              <div>
+                <h2 className='center'>My Skills</h2>
+                <SkillsPage />
+              </div>  
+            </section>       
+            <section id='project'>
+              <ProjectPage />
+
+            </section>       
+            <section id='resume'>
+              <ResumePage />
+
+            </section>       
+            <section id='contact'>
+             <ContactPage />
+
+            </section>       
+            
+                    
           </div>
         // </>
       }
