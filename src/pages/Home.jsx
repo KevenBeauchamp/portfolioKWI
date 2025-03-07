e57d0717b4c036e33a4347a92b0eb69b1c508739
Keven Beauchamp <kevenbeauchamp96@gmail.com>
2025-03-06 23:13:50 -0500

    modif home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,16 @@ import { motion, useMotionValue, useTransform, animate } from "framer-motion";
   import classes from "../css/Home.module.css"
 import { useEffect } from "react";
 import pdf from "../assets/Keven_BEAUCHAMP.pdf";
+import reactpicture from "../assets/react.webp";
+import javascript_picture from "../assets/js.webp";
+import databasePicture from "../assets/database.png";
+import pythonPicture from "../assets/python.png";
+import nodejsPicture from "../assets/nodejs.png";
+import phpPicture from "../assets/php.png";
+import javaPicture from "../assets/java.png";
+import igPicture from "../assets/instagram.png";
+import linkedinPicture from "../assets/linkedin.png";
+import githubPicture from "../assets/github.png";
 export default function HomePage(){
     const text = "Software Developer".split(" ");
     const textIndex = useMotionValue(0);
@@ -53,40 +63,88 @@ export default function HomePage(){
     });
   }, []);
     return(
-       
-            <motion.div
-                initial= {{opacity:0}}
-                animate={{opacity:1}}
-                transition={{delay:0.5, duration:0.5}}            
-                className={classes.content}
-            >
+            <div>
+              <motion.div
+                  initial= {{opacity:0}}
+                  animate={{opacity:1}}
+                  transition={{delay:0.5, duration:0.5}}            
+                  className={classes.content}
+              >
+                  
+                  <h1>Hi, I'm <motion.span
+                  initial= {{opacity:0}}
+                          animate={{
+                              fontSize: "50px", color: '#0000ff',opacity:1
+                          }}
+                          transition={{delay:1.7, duration:1}}
+                  >Keven Beauchamp</motion.span></h1> 
+                  <h3>  I am  &nbsp; 
+                      <motion.span className="inline">{displayText}</motion.span>
+                      &nbsp;  based in USA</h3>
+                  <p>    &nbsp;
+                  <motion.button
+                    onClick={onButtonClick}
+                      whileTap={{ scale: 0.9 }}
+                      whileHover={{
+                      scale: 1.1,
+                      backgroundColor: "#0000ff",
+                      color: "black",
+                      }}
+                      transition={{ bounceDamping: 10, bounceStiffness: 600 }}
+                      className="bg-emerald-600 w-1/2 py-4 rounded-lg text-2xl text-gray-100 font-light tracking-wide"
+                  >
+                      Download CV
+                  </motion.button>
+                  </p>
+              </motion.div>
+              <div className={classes.partThree}>
+                  <h3>Certificate</h3>
+                  <p>Comptia Network +</p>
+                  <p>IBM Full Stack Software Developer Professional Certificate</p>
+                  <p>CCNA</p>
+                  <p>Azure administration</p>
+              </div>
+              <div className={classes.partTwo}>
+                <div className={classes.skill}>
+                      <div className={classes.skillOne}>
+                        <img src={javascript_picture}  width="25px" height="25px" alt="" srcset="" />
+                      </div>
+                      <div className={classes.skillOne}>
+                        <img src={nodejsPicture} width="25px" height="25px" alt="" srcset="" />
+
+                      </div>
+                      <div className={classes.skillOne}>
+                        <img src={phpPicture} width="25px" height="25px" alt="" srcset="" />
+
+                      </div>
+                      <div className={classes.skillOne}>
+                        <img src={pythonPicture} width="25px" height="25px" alt="" srcset="" />
+
+                      </div>
+                      <div className={classes.skillOne}>
+                        <img src={javaPicture} width="25px" height="25px" alt="" srcset="" />
+
+                      </div>
+                      <div className={classes.skillOne}>
+                       <img src={reactpicture} width="25px" height="25px" alt="" srcset="" />
+
+                      </div>
+                      <div className={classes.skillOne}>
+                        <img src={javascript_picture} width="25px" height="25px" alt="" srcset="" />
+
+                      </div>
+                </div>
+                <div>
+                      <div className={classes.social}>
+                        <span><a href="http://"><img src={igPicture} alt="" srcset="" width="25px" /></a></span>
+                        <span><a href="https://www.linkedin.com/in/keven-beauchamp-276551298" target="_blank"><img src={linkedinPicture} alt="" srcset="" width="25px"/></a></span>
+                        <span><a href="https://github.com/KevenBeauchamp" target="_blank"><img src={githubPicture} alt="" srcset="" width="25px" /></a></span>
+                      </div>
+                </div>
                 
-                <h1>Hi, I'm <motion.span
-                initial= {{opacity:0}}
-                        animate={{
-                            fontSize: "50px", color: '#0000ff',opacity:1
-                        }}
-                        transition={{delay:1.7, duration:1}}
-                >Keven Beauchamp</motion.span></h1> 
-                <h3>  I am  &nbsp; 
-                    <motion.span className="inline">{displayText}</motion.span>
-                    &nbsp;  based in USA</h3>
-                <p>    &nbsp;
-                <motion.button
-                  onClick={onButtonClick}
-                    whileTap={{ scale: 0.9 }}
-                    whileHover={{
-                    scale: 1.1,
-                    backgroundColor: "#0000ff",
-                    color: "black",
-                    }}
-                    transition={{ bounceDamping: 10, bounceStiffness: 600 }}
-                    className="bg-emerald-600 w-1/2 py-4 rounded-lg text-2xl text-gray-100 font-light tracking-wide"
-                >
-                    Download CV
-                </motion.button>
-                </p>
-            </motion.div>
+              </div>
+              
+            </div>
         
     )
 }
